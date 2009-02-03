d43ed7b31c053e7dda2c75b3df5e0e89bb8e9260
Leah Culver <leah.culver@gmail.com>
2009-02-03 22:25:37 +0000

    motion event display on main page

--- __init__.py
+++ __init__.py
@@ -156,17 +156,43 @@ class RemoteObject(object):
 
 class User(RemoteObject):
     fields = {
+        'id':           basestring,
         'displayName':  basestring,
-        'email': basestring,
-        'userpic': basestring,
-        'uri':   basestring,
+        'email':        basestring,
+        'userpic':      basestring,
+        'uri':          basestring,
+        'interests':    basestring, 
+        'object-type':  basestring,
+        'aboutMe':      basestring, 
     }
 
     @property
     def permalink(self):
+        ## TODO link to typepad profile?
         return self.uri
 
-# crappy temp Group stuff - Leah
+# crappy temp stuff
+
+class Object(RemoteObject):
+    fields = {
+        'id':        basestring,
+        #'control': Control,
+        'title':     basestring,
+        'content':   basestring,
+        'link':      basestring,
+        'published': datetime,
+        'updated':   datetime,
+        'authors':   User,
+    }
+
+class Entry(RemoteObject):
+    fields = {
+        #'verbs': Verb,
+        'id':     basestring,
+        'actor':  User,
+        'object': Object,
+    }
+
 class Group(RemoteObject):
     fields = {
         'displayName':  basestring,
@@ -178,6 +204,16 @@ class GroupUsers(RemoteObject):
         'entries': User,
     }
 
+class GroupEvents(RemoteObject):
+    fields = {
+        'updated': datetime,
+        'title': basestring,
+        'authors': User,
+        'link': basestring, 
+        'entries': Entry,
+    }
+# end crappy temp stuff
+'''
 class Entry(RemoteObject):
     fields = {
         'slug':      basestring,
@@ -213,6 +249,7 @@ class Blog(RemoteObject):
         'authors':  User,
         'entries':  Entry,
     }
+'''
 
 class Client(object):
     def __init__(self, *args, **kwargs):
