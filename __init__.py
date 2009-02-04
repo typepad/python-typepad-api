aabbee5f24fc48c61d83dde15777c1d818a3395f
Mark Paschal <mark@sixapart.com>
2009-02-04 04:50:01 +0000

    Switch to Field codec objects

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from typepad.dataobject import DataObject
+from typepad.dataobject import *
 from typepad.remote import RemoteObject
 from typepad.asset import *
--- dataobject.py
+++ dataobject.py
@@ -4,57 +4,88 @@ import logging
 from datetime import datetime
 import time
 
+__all__ = ('Field', 'SetField', 'ObjectField', 'StringField', 'IntField', 'DatetimeField', 'DataObject')
+
+
+class Field(object):
+    def decode(self, value):
+        raise NotImplementedError('Decoding is not generically implemented')
+
+    def encode(self, value):
+        raise NotImplementedError('Encoding is not generically implemented')
+
+class SetField(Field):
+    def __init__(self, fld):
+        self.fld = fld
+
+    def decode(self, value):
+        return [self.fld.decode(v) for v in value]
+
+    def encode(self, value):
+        return [self.fld.encode(v) for v in value]
+
+class ObjectField(Field):
+    def __init__(self, cls):
+        self.cls = cls
+
+    def decode(self, value):
+        if not isinstance(value, dict):
+            raise ValueError('HMM THIS VALUE IS A %s (%r)' % (type(value), value))
+        return self.cls.from_dict(value)
+
+    def encode(self, value):
+        assert isinstance(value, self.cls)
+        return value.to_dict()
+
+class StringField(Field):
+    def decode(self, value):
+        assert isinstance(value, basestring)
+        return value
+
+    def encode(self, value):
+        assert isinstance(value, basestring)
+        return value
+
+class IntField(Field):
+    def decode(self, value):
+        assert isinstance(value, int)
+        return value
+
+    def encode(self, value):
+        assert isinstance(value, int)
+        return value
+
+class DatetimeField(Field):
+    def decode(self, value):
+        return datetime(*(time.strptime(value, '%Y-%m-%dT%H:%M:%SZ'))[0:6])
+
+    def encode(self, value):
+        assert isinstance(value, datetime)
+        return value.isoformat()
+
+
 class DataObject(object):
     fields = {}
 
     def __init__(self, **kwargs):
         self._id = None
         self.parent = None
-        self.update(**kwargs)
-
-    def update(self, **kwargs):
-        for field_name, field_class in self.fields.iteritems():
-            value = kwargs.get(field_name)
-            # TODO: reuse child objects as appropriate
-            if isinstance(value, list) or isinstance(value, tuple):
-                new_value = []
-                for item in value:
-                    o = field_class(**item)
-                    o.parent = self
-                    new_value.append(o)
-                value = new_value
-            elif isinstance(value, dict):
-                value = field_class(**value)
-                value.parent = self # e.g. reference to blog from entry
-            elif field_class is datetime:
-                if value is not None:
-                    value = datetime(*(time.strptime(value, '%Y-%m-%dT%H:%M:%SZ'))[0:6])
-            setattr(self, field_name, value)
-
-    def serialize_value(self, value):
-        if isinstance(value, DataObject):
-            value = value.to_dict()
-        elif isinstance(value, datetime):
-            value = value.isoformat()
-        elif isinstance(value, dict):
-            pass
-        elif isinstance(value, list):
-            newlist = []
-            for x in value:
-                newlist.append(self.serialize_value(x))
-            value = newlist
-        elif not isinstance(value, basestring):
-            value = str(value)
-        return value
+        self.__dict__.update(kwargs)
 
     def to_dict(self):
         data = {}
-        for field_name, field_class in self.fields.iteritems():
+        for field_name, field in self.fields.iteritems():
             value = getattr(self, field_name)
             if value is not None:
-                data[field_name] = self.serialize_value(value)
+                data[field_name] = field.encode(value)
         return data
 
     @classmethod
     def from_dict(cls, data):
-        return cls(**data)
+        self = cls()
+        for field_name, field in cls.fields.iteritems():
+            value = data.get(field_name)
+            if value is not None:
+                value = field.decode(value)
+            setattr(self, field_name, value)
+        return self
