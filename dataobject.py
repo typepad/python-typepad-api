f2dc1ca607ebb40707c4e32776a49e0caf60200d
Mark Paschal <mark@sixapart.com>
2009-02-15 09:05:36 +0000

    Pull fields from class properties (but just put them in a 'fields' dict like before)

--- dataobject.py
+++ dataobject.py
@@ -1,7 +1,19 @@
 import logging
+import typepad.fields
+
+class DataObjectMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        fields = {}
+        for name, field in attrs.iteritems():
+            if isinstance(field, typepad.fields.Something):
+                fields[name] = field
+        for name in fields:
+            del attrs[name]
+        attrs['fields'] = fields
+        return super(DataObjectMetaclass, cls).__new__(cls, name, bases, attrs)
 
 class DataObject(object):
-    fields = {}
+    __metaclass__ = DataObjectMetaclass
 
     def __init__(self, **kwargs):
         self._id = None
