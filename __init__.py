f964a7eb7c0e85f21c33ab7b66e7a23537341b38
Leah Culver <leah.culver@gmail.com>
2009-02-18 23:52:46 +0000

    oops, added models for OAuthClient

--- __init__.py
+++ __init__.py
@@ -45,3 +45,57 @@ class OAuthAuthentication(httplib2.Authentication):
 
 httplib2.AUTH_SCHEME_CLASSES['oauth'] = OAuthAuthentication
 httplib2.AUTH_SCHEME_ORDER[0:0] = ('oauth',)  # unshift onto front
+
+class OAuthClient(oauth.OAuthClient):
+    """
+        An httplib2 OAuth client.
+    """
+    consumer = None
+    request_token_url = None
+    access_token_url = None
+    authorization_url = None
+    callback_url = None
+    
+    def set_consumer(self, key, secret):
+        self.consumer = oauth.OAuthConsumer(
+            key = key,
+            secret = secret,
+        )
+
+    def set_token_from_string(self, token_str):
+        self.token = oauth.OAuthToken.from_string(token_str)
+
+    def fetch_request_token(self):
+        # -> OAuthToken
+        h = httplib2.Http()
+        req = oauth.OAuthRequest.from_consumer_and_token(
+            self.consumer,
+            http_method = 'GET',
+            http_url = self.request_token_url,
+        )
+        req.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), self.consumer, None)
+        resp, content = h.request(req.to_url(), method=req.get_normalized_http_method())
+        self.token = oauth.OAuthToken.from_string(content)
+        return self.token
+
+    def fetch_access_token(self, request_token_str=None):
+        # -> OAuthToken
+        h = httplib2.Http()
+        req = oauth.OAuthRequest.from_consumer_and_token(
+            self.consumer,
+            token = self.token,
+            http_url = self.access_token_url,
+        )
+        req.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), self.consumer, self.token)
+        resp, content = h.request(req.to_url(), method=req.get_normalized_http_method())
+        self.token = oauth.OAuthToken.from_string(content)
+        return self.token
+
+    def authorize_token(self):
+        h = httplib2.Http()
+        req = oauth.OAuthRequest.from_token_and_callback(
+            self.token,
+            callback=self.callback_url,
+            http_url=self.authorization_url,
+        )
+        return req.to_url()
