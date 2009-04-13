fefd28601fdb418229bc44b50df4aaa56bd7c002
Mark Paschal <mark@sixapart.com>
2009-04-13 17:54:08 +0000

    Some docs

--- fields.py
+++ fields.py
@@ -4,7 +4,25 @@ import remoteobjects.fields
 from remoteobjects.fields import *
 
 class Link(remoteobjects.fields.Link):
+
+    """A `TypePadObject` property representing a 'link' from one TypePad API
+    object to another.
+
+    This `Link` works like `remoteobjects.fields.Link`, but builds links using
+    the TypePad API URL scheme. That is, a `Link` on `/asset/1.json` called
+    `events` links to `/asset/1/events.json`.
+
+    """
+
     def __get__(self, instance, owner):
+
+        """Generates the `TypePadObject` representing the target of this
+        `Link` object.
+
+        This `__get__` implementation implements the `../x/target.json` style
+        URLs used in the TypePad API.
+
+        """
         try:
             if instance._location is None:
                 raise AttributeError('Cannot find URL of %s relative to URL-less %s' % (type(self).__name__, owner.__name__))
