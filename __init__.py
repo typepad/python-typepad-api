cdf515362b6838c28f9cce79aeaa5a6590a4927a
Mark Paschal <mark@sixapart.com>
2009-04-20 22:00:47 +0000

    Stub docstrings for batchhttp and typepad packages Add package metadata

--- __init__.py
+++ __init__.py
@@ -1,3 +1,16 @@
+"""
+
+typepad provides connectivity to the TypePad API through remote objects.
+
+"""
+
+__version__ = '1.0'
+__date__ = '20 April 2009'
+__author__ = 'Six Apart, Ltd.'
+__credits__ = """Brad Choate
+Leah Culver
+Mark Paschal"""
+
 from remoteobjects import RemoteObject, ListObject
 import batchhttp.client
 
