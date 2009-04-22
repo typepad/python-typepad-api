2fba85e89f1a07b5910402f9290f3bd666ac90a2
Brad Choate <brad@bradchoate.com>
2009-04-22 22:58:07 +0000

    Removed extra ','.

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ API.
 
 __version__ = '1.0'
 __date__ = '20 April 2009'
-__author__ = 'Six Apart, Ltd.'
+__author__ = 'Six Apart Ltd.'
 __credits__ = """Brad Choate
 Leah Culver
 Mark Paschal"""
--- doc/conf.py
+++ doc/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'typepad'
-copyright = u'2009, Six Apart, Ltd.'
+copyright = u'2009, Six Apart Ltd.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -173,7 +173,7 @@ htmlhelp_basename = 'typepaddoc'
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'typepad.tex', u'typepad Documentation',
-   u'Six Apart, Ltd.', 'manual'),
+   u'Six Apart Ltd.', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
