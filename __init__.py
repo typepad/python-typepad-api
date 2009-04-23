ca2ab244ebd4b8611c65353fe6163a0fffd90fa5
Mark Paschal <mark@sixapart.com>
2009-04-23 22:53:33 +0000

    Provide microscope cookies when asked by settings

--- __init__.py
+++ __init__.py
@@ -15,13 +15,27 @@ __credits__ = """Brad Choate
 Leah Culver
 Mark Paschal"""
 
-from remoteobjects import RemoteObject, ListObject
-import batchhttp.client
+import httplib2
 
+import batchhttp.client
+from remoteobjects import RemoteObject, ListObject
 from typepad.oauthclient import *
 
 class TypePadClient(batchhttp.client.BatchClient, OAuthHttp):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        self.cookies = dict()
+        super(TypePadClient, self).__init__(*args, **kwargs)
+
+    def request(self, uri, method="GET", body=None, headers=None, redirections=httplib2.DEFAULT_MAX_REDIRECTS, connection_type=None):
+        if self.cookies:
+            if headers is None:
+                headers = {}
+            else:
+                headers = dict(headers)
+            cookies = ['='.join((key, value)) for key, value in self.cookies.items()]
+            headers['cookie'] = '; '.join(cookies)
+        return super(TypePadClient, self).request(uri, method, body, headers, redirections, connection_type)
 
 client = TypePadClient()
 
