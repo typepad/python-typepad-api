a307c602aabc066b474a23509d9ea5b13ce608e3
Mark Paschal <markpasc@markpasc.org>
2010-05-17 17:54:37 -0700

    Add some checks for whether our environment is buildworthy, and a 'package' target to build everything

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 import os
-import os.path
+from os.path import join, dirname, abspath
+import re
 from shutil import rmtree
 import tempfile
 import zipfile
@@ -10,8 +11,45 @@ from httplib2 import Http
 
 
 def fill_environment():
-    env.name = local('python setup.py --name').strip()
-    env.version = local('python setup.py --version').strip()
+    env.dist_name = local('python setup.py --name').strip()
+    env.dist_version = local('python setup.py --version').strip()
+
+def check_active_package():
+    require('dist_name')
+
+    # Are we set up to be us?
+    py_file = abspath(local("python -c 'import %(dist_name)s; print %(dist_name)s.__file__'" % env).strip())
+    if dirname(py_file) != join(dirname(abspath(__file__)), env.dist_name):
+        abort("The local %(dist_name)s package is not the %(dist_name)s package in the active environment: %(badpath)s"
+            % {'dist_name': env.dist_name, 'badpath': dirname(py_file)})
+
+def check_version(ver):
+    require('dist_name', 'dist_version')
+
+    # 1. the setup.py version
+    if env.dist_version != ver:
+        abort("Version in setup.py is %r, not %r" % (env.dist_version, ver))
+
+    # 2. the __init__.py version
+    py_ver = local("python -c 'import %(dist_name)s; print %(dist_name)s.__version__'" % env).strip()
+    if py_ver != ver:
+        abort("Version in %s/__init__.py is %r, not %r" % (env.dist_name, py_ver, ver))
+
+    # 3. the doc/conf.py version
+    doc_ver = local("""python -c 'data = {}; execfile("doc/conf.py", data); print data["release"]'""").strip()
+    if doc_ver != ver:
+        abort("Release version in doc/conf.py is %r, not %r" % (doc_ver, ver))
+
+def check_changes(ver):
+    # Is our version already in CHANGES.rst?
+    with open('CHANGES.rst') as changes_file:
+        found = False
+        for line in changes_file:
+            if re.match(r'^%s\b' % re.escape(ver), line):
+                found = True
+                break
+        if not found:
+            abort("Version %s doesn't appear in the CHANGES.rst" % (ver,))
 
 def sdist():
     local('python setup.py sdist')
@@ -49,6 +87,21 @@ def docs(zipname):
         zip = zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED)
         for root, dirs, files in os.walk(target_dir):
             for filename in files:
-                zip.write(os.path.join(root, filename))
+                zip.write(join(root, filename))
     finally:
         rmtree(target_dir)
+
+def package(ver):
+    fill_environment()
+
+    check_active_package()
+    check_version(ver)
+    check_changes(ver)
+
+    # TODO: check that we needn't regenerate the python module again?
+
+    # rebuild the README
+    local('python doc/readme_from_docstring.py')
+
+    sdist()  # makes dist/name-version.tar.gz
+    docs('dist/%(dist_name)s-%(dist_version)s-docs.zip' % env)
