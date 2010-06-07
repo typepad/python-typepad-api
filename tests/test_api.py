76d0d23e3553410d8863f02e7b271d7b0d9d1b9b
Mark Paschal <markpasc@markpasc.org>
2010-06-07 11:50:53 -0700

    Test Relationship.block()

--- tests/test_api.py
+++ tests/test_api.py
@@ -45,6 +45,7 @@ import traceback
 import unittest
 from urlparse import urlparse
 
+import httplib2
 import mox
 from oauth.oauth import OAuthConsumer, OAuthToken
 import simplejson as json
@@ -188,7 +189,7 @@ class TestHttp(unittest.TestCase):
         try:
             req = requests[key]
         except KeyError:
-            raise Exception('No such mock request %s' % (callername,))
+            raise Exception('No such mock request %s' % key)
 
         mock = utils.mock_http(*req)
         mock.endpoint = 'http://api.typepad.com'
@@ -569,6 +570,42 @@ class TestRelationship(unittest.TestCase):
         self.assert_(not r.is_member())
         self.assert_(not r.is_admin())
         self.assert_(r.is_blocked())
+
+    def test_rel_type_updaters(self):
+        real_typepad_client = typepad.client
+        typepad.client = mox.MockObject(httplib2.Http)
+        try:
+            resp = httplib2.Response({
+                'status':           200,
+                'etag':             '7',
+                'content-type':     'application/json',
+                'content-location': 'http://127.0.0.1:8000/relationships/6r00d83451ce6b69e20120a81fb3a4970c/status.json',
+            })
+            typepad.client.request(
+                uri='http://127.0.0.1:8000/relationships/6r00d83451ce6b69e20120a81fb3a4970c/status.json',
+                headers={'accept': 'application/json'},
+            ).AndReturn((resp, """{"types": ["tag:api.typepad.com,2009:Member"]}"""))
+            resp = httplib2.Response({
+                'status':           200,
+                'etag':             '9',
+                'content-type':     'application/json',
+                'content-location': 'http://127.0.0.1:8000/relationships/6r00d83451ce6b69e20120a81fb3a4970c/status.json',
+            })
+            typepad.client.request(
+                uri='http://127.0.0.1:8000/relationships/6r00d83451ce6b69e20120a81fb3a4970c/status.json',
+                method='PUT',
+                headers={'if-match': '7', 'accept': 'application/json', 'content-type': 'application/json'},
+                body=mox.Func(json_equals_func({"types": ["tag:api.typepad.com,2009:Blocked"]})),
+            ).AndReturn((resp, """{"types": ["tag:api.typepad.com,2009:Blocked"]}"""))
+            mox.Replay(typepad.client)
+
+            r = typepad.Relationship.get('http://127.0.0.1:8000/relationships/6r00d83451ce6b69e20120a81fb3a4970c.json')
+            r.block()
+
+            mox.Verify(typepad.client)
+
+        finally:
+            typepad.client = real_typepad_client
 
 
 class TestUserAndUserProfile(unittest.TestCase):
